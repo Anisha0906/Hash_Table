9a27f1f4dedd4c8e4ef7ca38a8cc8793a1921b75
Anisha0906 <anishadas880@gmail.com>
2023-01-19 19:08:13 +0530

    [Anisha]Added:Ability to find frequency of words in a sentence

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTableBuilder hashTable = new HashTableBuilder();
+
+            Console.WriteLine($"Frequency of word is {hashTable.FindFrequencyOfWord()}");
+            Console.ReadLine();
+        }
+    }
+}
